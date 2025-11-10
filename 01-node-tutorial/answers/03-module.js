6473740fbc01edc6c35e267f8ee6267e364728a1
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-11-10 00:55:38 -0500

    03-modules.js, 04-names,js, and 05-utils.js added

---
+++ 01-node-tutorial/answers/03-module.js
@@ -0,0 +1,7 @@
+const names = require("./04-names");
+const greeting = require("./05-utils");
+
+greeting(names.bob);
+greeting(names.lily);
+greeting(names.ted);
+greeting(names.laura);
---
+++ 01-node-tutorial/answers/04-names.js
@@ -0,0 +1,6 @@
+const bob = "Bob";
+const lily = "Lily";
+const ted = "Ted";
+const laura = "Laura";
+
+module.exports = { bob, lily, ted, laura };
