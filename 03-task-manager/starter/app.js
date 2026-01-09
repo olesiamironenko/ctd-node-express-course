dcef70462c4f43bcbb1c78bee134d679ac755814
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2026-01-08 21:35:05 -0500

    task model and task controllers added

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -7,13 +7,10 @@ const connectDB = require("./db/connect");
 require("dotenv").config();
 
 // middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
 // routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
 
 // app.get("/api/v1/tasks"); //        - get all the tasks
--- 03-task-manager/starter/models/task.js
+++ 03-task-manager/starter/models/task.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "name must be provided"],
+    trim: true,
+    maxlength: [20, "name must be no longer than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
