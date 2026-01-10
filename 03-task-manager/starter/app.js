07b7e781ad233018acff92d8879bb1511076424f
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2026-01-10 15:20:21 -0500

    error handling and port added

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+
 const tasks = require("./routes/tasks");
-
 const connectDB = require("./db/connect");
-
-require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
 app.use(express.static("./public"));
@@ -12,14 +14,10 @@ app.use(express.json());
 
 // routes
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-// app.get("/api/v1/tasks"); //        - get all the tasks
-// app.post("/api/v1/tasks"); //       - create a new task
-// app.get("/api/v1/tasks/:id"); //    - get single task by id
-// app.patch("/api/v1/tasks/:id"); //  - update task
-// app.delete("/api/v1/tasks/:id"); // - delete task
-
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
